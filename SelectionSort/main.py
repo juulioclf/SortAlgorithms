16bc9093c8c1b0ca19ef9894f6269e74261bbab7
Júlio Ferreira <juliolima731@gmail.com>
2024-06-10 00:13:49 -0400

    Insertion and Selection

--- SelectionSort/main.py
+++ SelectionSort/main.py
@@ -1,10 +1,15 @@
 def selection_sort(array):
     n = len(array)
 
-    for i in range(n-1):
-        for j in range(i, n-1-i):
-            print(array[i], array[j])
+    for i in range(n):
+        min_ind = i
+        
+        for j in range(i+1, n):
+            if (array[j] < array[min_ind]):
+                min_ind = j
 
+        array[i], array[min_ind] = array[min_ind], array[i]
+            
 
 array = [5,1,4,2,8,9]
 selection_sort(array)
